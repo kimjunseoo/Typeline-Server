db419b757945ba6641038d14d10c398eeb1143e9
kimjunseoo <zz0905k@naver.com>
2023-06-09 15:14:20 +0900

    Init: MCS

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ScheduleModule } from './schedule/schedule.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, ScheduleModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
