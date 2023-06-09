7d9bf918a7fe2d59c2acbe80f502b1f77a5834d4
kimjunseoo <zz0905k@naver.com>
2023-06-09 15:59:35 +0900

    Feat: API registerUserInfo

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,16 @@
+import { createUserInfo } from './dto/create-userInfo.dto';
 import { UserService } from './user.service';
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 
 @Controller('user')
 export class UserController {
     constructor(private userService: UserService){
 
+    }
+
+    @Post()
+    registerUserInfo(@Body() userInfo: createUserInfo){
+        return `${userInfo}`;
     }
 
 }
