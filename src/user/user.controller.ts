4cca01f82c825cbede7787e4772b295e5232b7ca
kimjunseoo <zz0905k@naver.com>
2023-06-13 15:20:49 +0900

    Feat: user service method
    registerUserInfo, deleteUserInfo

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -16,17 +16,18 @@ export class UserController {
     @ApiOperation({ summary: '회원 등록', description: '구글 소셜 로그인을 통해 최초 가입 시 진행하는 유저 등록' })
     @ApiResponse({
         status: 201,
+        type: createUserInfoDTO,
         description: '회원 등록 성공',
     })
     @ApiResponse({ status: 500, description: 'Server Error'})
     @Post()
-    async registerUserInfo(@Body() userInfo: createUserInfoDTO): Promise<void>{
+    async registerUserInfo(@Body() userInfo: createUserInfoDTO): Promise<object>{
         return this.userService.registerUserInfo(userInfo);
     }
 
     @ApiOperation({ summary: '회원 탈퇴', description: '가입한 유저 정보를 삭제 / 탈퇴' })
     @Delete()
-    async deleteUserInfo(@Body() userInfo: deleteUserInfoDTO): Promise<void>{
-
+    async deleteUserInfo(@Body() userInfo: deleteUserInfoDTO): Promise<object>{
+        return this.userService.deleteUserInfo(userInfo)
     } 
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,8 @@
-import { Body, Injectable } from '@nestjs/common';
+import { BadRequestException, Body, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import {  User } from '@prisma/client';
 import { createUserInfoDTO } from './dto/createUserInfo.dto';
+import { deleteUserInfoDTO } from './dto/deleteUserInfo.dto';
 
 @Injectable()
 export class UserService {
@@ -10,6 +11,53 @@ export class UserService {
     }
 
     async registerUserInfo(userInfo : createUserInfoDTO){
-        await this.prismaservice.user.create({ data: userInfo });
+
+        //주어진 정보가 유효하다면, user 를 생성하고 해당 user 정보를 반환함.
+        //주어진 user id가 이미 존재한다면, 해당 user id가 이미 존재한다는 예외를 반환함.
+        const userId = userInfo.user_id;
+
+        const user = await this.prismaservice.user.findUnique({
+            where: {
+                user_id: userId
+            }
+        });
+
+        if(!user){
+            const genUser = await this.prismaservice.user.create({ data: userInfo });
+
+            return genUser;
+        } else {
+            throw new BadRequestException(`User ID ${userId} already exists. User IDs cannot be duplicated.`)
+        }
+        
+    }
+    
+    async deleteUserInfo(userInfo: deleteUserInfoDTO){
+        const userId = userInfo.user_id;
+
+        const user = await this.prismaservice.user.findUnique({
+            where: {
+                user_id: userId
+            }
+        })
+
+        if(!user){
+            throw new NotFoundException(`ID ${userId} requested for deletion was not found.`);
+            
+        } else{
+
+            await this.prismaservice.user.delete({
+                where: {
+                    user_id: userId
+                }
+            });
+
+            return user;
+
+        }
+
+
+        //주어진 정보가 유효하다면, user 를 삭제하고 해당 user 정보를 반환함.
+        //주어진 user id가 존재하지 않는다면, 해당 user id가 존재하지 않는다면 예외를 반환함.
     }
 }
