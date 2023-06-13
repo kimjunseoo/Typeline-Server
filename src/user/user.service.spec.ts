62be7e08942cde1f96605f690e9ddd13632478b6
kimjunseoo <zz0905k@naver.com>
2023-06-13 14:25:41 +0900

    test: fix user test case

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -18,7 +18,7 @@ describe('UserService', () => {
   });
 
   describe('registerUserInfo', () => {
-    it('주어진 정보가 유효하다면, user 를 생성하고 해당 user 정보와 생성 성공 문구를 json 형태로 반환함.', () => {
+    it('주어진 정보가 유효하다면, user 를 생성하고 해당 user 정보를 반환함.', () => {
 
     });
     it('주어진 user id가 이미 존재한다면, 해당 user id가 이미 존재한다는 예외를 반환함.', () => {
@@ -27,7 +27,7 @@ describe('UserService', () => {
   });
 
   describe('deleteUserInfo', () => {
-    it('주어진 정보가 유효하다면, user 를 삭제하고 해당 user 정보와 삭제 성공 문구 json 형태로 반환함.', () => {
+    it('주어진 정보가 유효하다면, user 를 삭제하고 해당 user 정보를 반환함.', () => {
 
     });
     it('주어진 user id가 존재하지 않는다면, 해당 user id가 존재하지 않는다면 예외를 반환함.', () => {
