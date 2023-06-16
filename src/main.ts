a884eb4667681f3bea0354b4f8d71850e3ab48b4
kimjunseoo <zz0905k@naver.com>
2023-06-16 20:07:29 +0900

    fix: cors on localhost

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import 'dotenv/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: process.env.CORS_ENABLE_URL,
+    maxAge: 86400
+  });
   const config = new DocumentBuilder()
     .setTitle('Typeline')
     .setDescription('Description of Typeline Server API')
